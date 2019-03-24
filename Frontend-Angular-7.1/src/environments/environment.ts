3b411d842f7e3d8253ef3b6aa17308af0e48c8b0
Danil Rodin <danre1@bk.ru>
2019-03-24 07:27:48 +0300

    rename folders

--- Frontend-Angular-7.1/src/environments/environment.ts
+++ Frontend-Angular-7.1/src/environments/environment.ts
@@ -2,6 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+export const api = "localhost:8000";
 export const environment = {
   production: false
 };
